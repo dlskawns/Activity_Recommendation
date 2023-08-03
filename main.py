851aa0943a1e217b1913b818c0f03515c6ec6e74
David Lee <fabric_lee@naver.com>
2023-08-03 17:48:45 +0900

    prompt simplify, fixed output format

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 
-from recom import cfg, Act_Rec
+from recom_new2 import Act_Rec
 import os
 import time 
 import pandas as pd
+import langchain
+from langchain.cache import InMemoryCache
 
 
- 
 def main(input_value):
     """
     추천 API 결과 값을 통해 처리
     input_value: 추천된 책 no.
     """
+    
     # 도서 데이터셋 경로 설정
     data_path = 'MF_BookDB.ver4.63.csv'
     book_data = pd.read_csv(data_path)
@@ -18,9 +20,10 @@ def main(input_value):
     # 추천 API 직후 추천된 도서에 따른 정보 가져오기
     major = book_data['중분류'][input_value]            # 관련분야(학과)
     book_text = book_data['Bard 책 요약'][input_value]  # 책 내용 요약
-    
+    print(major)
+    print(book_text)
     # 활동 추천 인스턴스 생성
-    book_act_rec = Act_Rec(cfg)
+    book_act_rec = Act_Rec()
 
     # 일반활동 추천
     general_act = book_act_rec.general_recommend(major)
@@ -31,6 +34,7 @@ def main(input_value):
 
 
 if __name__ == '__main__':
+    
     start = time.time()
     main(int(input()))
     end = time.time()
